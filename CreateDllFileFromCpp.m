c2b9acdb37ddceb62f188e43c8b33931901dfd28
Maarten Afschrift <maarten.afschrift@kuleuven.be>
2022-04-28 20:52:19 +0200

    updates that works with new opensim-ad version

--- CreateDllFileFromCpp.m
+++ CreateDllFileFromCpp.m
@@ -15,7 +15,7 @@ function [] = CreateDllFileFromCpp(CppDir,Name,OsimSource,OsimBuild,DllPath,ExtF
 %       (for inverse dynamics is this tyically ndof*3 [q qd qdd].
 %
 % authors: Maarten Afschrift (KU Leuven)
-Compiler = 'Visual Studio 14 2015 Win64';   % default compiler
+Compiler = 'Visual Studio 15 2017 Win64';   % default compiler
 if ~isempty(varargin)
     Compiler = varargin{1};
 end
@@ -23,138 +23,130 @@ end
 if ~exist(fullfile(DllPath,[Name '.dll']),'file')
     % path info
     PathStart = pwd;
-    
+
     %% OpenSim source - external function
     % create folder in the opensim source folder
     ExtFuncPath = fullfile(OsimSource,'OpenSim\External_Functions',Name);
-    mkdir(ExtFuncPath);
-    
-    % create the CMakeList file
-    fid = fopen(fullfile(ExtFuncPath,'CMakeLists.txt'),'wt');
-    fprintf( fid, '%s\n', ['set(TEST_TARGET ' Name ')']);
-    fprintf( fid, '%s\n', 'add_executable(${TEST_TARGET} ${TEST_TARGET}.cpp)');
-    fprintf( fid, '%s\n', 'target_link_libraries(${TEST_TARGET} osimSimulation)');
-    fprintf( fid, '%s\n', 'set_target_properties(${TEST_TARGET} PROPERTIES');
-    fprintf( fid, '%s\n', '    FOLDER "External_Functions")');
-    fclose(fid);
-    % copy the cpp files
-    copyfile(fullfile(CppDir,[Name '.cpp']),fullfile(ExtFuncPath,[Name '.cpp']));
-    
-    % add the project to the current cmakelist of the the external projects
-    CmakeFile = fullfile(OsimSource,'OpenSim\External_Functions','CMakeLists.txt');
-%     ListExtFunc = importdata(CmakeFile);
-    tempCopy = fullfile(pwd,'CMakeListTempCopy.txt');
-    copyfile(CmakeFile,tempCopy);
-    delete(CmakeFile)
-    
-    % write the cmakefile again
-    % open file for reading
-    fidr = fopen(tempCopy,'r') ;
-    % open file for writing
-    fidw = fopen(CmakeFile,'w') ;
-    % while end of file has not been reached
-    while ( ~feof(fidr) )
-        % read line from reading file
-        str = fgets(fidr) ;
-        % match line to regular expression to determine if replacement needed
-        match = regexp(str,'endif()', 'match' ) ;
-        % if line is to be added
-        if (~isempty(match))
-            % added line
-            CharAdded = ['	add_subdirectory(' Name ')'];
-            fprintf(fidw,'%s\n',convertCharsToStrings(CharAdded));
-        end
-        % write line to writing file
-        fwrite(fidw,str);
-    end    
-    fclose(fidr);
-    fclose(fidw);
+    if ~isfolder(ExtFuncPath)
+        mkdir(ExtFuncPath);
 
-    %% Add new project to opensim build
-    % update visual studio project with cmake
-    % go to folder where you want to build the projects
-    % then run for example: "cmake C:\sourceTest -G "Visual Studio 14 2015
-    % Win64"
-    disp('... running cmake to update opensim build');
-    cd(OsimBuild);
-    TextCommand = ['cmake ' OsimSource ' -G "' Compiler '"'];
-    system(TextCommand);
-    
-    % or run it automatically using devnenv
-    % information: devenv mysln.sln /build Debug /project proj1
-    disp('... building project in visual studio');
-    ExeDevPath = fullfile(VSinstall,'Common7\IDE');
-    cd(ExeDevPath);
-    TextCommand = ['devenv.exe ' OsimBuild '\OpenSim.sln ' '/Build ' 'RelWithDebInfo ' '/Project ' Name ];
-    system(TextCommand);
-    
-    %% Create executable, foo.m and foo_jac
-    
-    % - first: create path with cgeneration info:
-    CgenDir1 = fullfile(OsimSource,'cgeneration',Name);
-    mkdir(CgenDir1);
-    
-    % - second: run the exectuables and copy foo.m
-    disp('... Running exectuble');
-    ExePath = fullfile(OsimBuild,'RelWithDebInfo');
-    cd(ExePath);
-    if exist(fullfile(ExePath,'foo.m'),'file')
-        delete(fullfile(ExePath,'foo.m'));
+        % create the CMakeList file
+        fid = fopen(fullfile(ExtFuncPath,'CMakeLists.txt'),'wt');
+        fprintf( fid, '%s\n', ['set(TEST_TARGET ' Name ')']);
+        fprintf( fid, '%s\n', 'add_executable(${TEST_TARGET} ${TEST_TARGET}.cpp)');
+        fprintf( fid, '%s\n', 'target_link_libraries(${TEST_TARGET} osimSimulation)');
+        fprintf( fid, '%s\n', 'set_target_properties(${TEST_TARGET} PROPERTIES');
+        fprintf( fid, '%s\n', '    FOLDER "External_Functions")');
+        fclose(fid);
+        % copy the cpp files
+        copyfile(fullfile(CppDir,[Name '.cpp']),fullfile(ExtFuncPath,[Name '.cpp']));
+
+        % add the project to the current cmakelist of the the external projects
+        CmakeFile = fullfile(OsimSource,'OpenSim\External_Functions','CMakeLists.txt');
+        tempCopy = fullfile(pwd,'CMakeListTempCopy.txt');
+        copyfile(CmakeFile,tempCopy);
+        delete(CmakeFile)
+        try
+            % try to run the full program. restore the original cmake file if
+            % an error occured
+
+            % write the cmakefile again
+            % open file for reading
+            fidr = fopen(tempCopy,'r') ;
+            % open file for writing
+            fidw = fopen(CmakeFile,'w') ;
+            % while end of file has not been reached
+            while ( ~feof(fidr) )
+                % read line from reading file
+                str = fgets(fidr) ;
+                % match line to regular expression to determine if replacement needed
+                match = regexp(str,'endif()', 'match' ) ;
+                % if line is to be added
+                if (~isempty(match))
+                    % added line
+                    CharAdded = ['	add_subdirectory(' Name ')'];
+                    fprintf(fidw,'%s\n',convertCharsToStrings(CharAdded));
+                end
+                % write line to writing file
+                fwrite(fidw,str);
+            end
+            fclose(fidr);
+            fclose(fidw);
+
+            %% Add new project to opensim build
+            % update visual studio project with cmake
+            % go to folder where you want to build the projects
+            % then run for example: "cmake C:\sourceTest -G "Visual Studio 14 2015
+            % Win64"
+            disp('... running cmake to update opensim build');
+            cd(OsimBuild);
+            %     TextCommand = ['cmake ' OsimSource ' -G "' Compiler '"'];
+            TextCommand = ['cmake ' OsimSource];
+            system(TextCommand);
+
+            % or run it automatically using devnenv
+            % information: devenv mysln.sln /build Debug /project proj1
+            disp('... building project in visual studio');
+            ExeDevPath = fullfile(VSinstall,'Common7\IDE');
+            cd(ExeDevPath);
+            TextCommand = ['devenv.exe ' OsimBuild '\OpenSim.sln ' '/Build ' 'RelWithDebInfo ' '/Project ' Name ];
+            system(TextCommand);
+
+            %% Create executable, foo.m and foo_jac
+
+            % - first: create path with cgeneration info:
+            CgenDir1 = fullfile(OsimSource,'cgeneration',Name);
+            mkdir(CgenDir1);
+
+            % - second: run the exectuables and copy foo.m
+            disp('... Running exectuble');
+            ExePath = fullfile(OsimBuild,'RelWithDebInfo');
+            cd(ExePath);
+            if exist(fullfile(ExePath,'foo.m'),'file')
+                delete(fullfile(ExePath,'foo.m'));
+            end
+            system([Name '.exe']);
+            copyfile(fullfile(ExePath,'foo.m'),fullfile(CgenDir1,'foo.m'));
+            delete(fullfile(ExePath,'foo.m'));
+
+            % - third: create the foo_jac.m using a matlab function
+            disp('... Creating foo_jac.c');
+            generate_foo_jac(nInputDll,CgenDir1);
+
+            %% Run cmake to build external function project
+
+            % First: create the cmake file
+            copyfile(fullfile(OsimSource,'cgeneration','CMakeLists.txt'),fullfile(CgenDir1,'CMakeLists.txt'));
+
+            % second: create folder for external functions
+            buildFolder = fullfile(ExtFuncs,Name,'build2');
+            mkdir( buildFolder);
+            mkdir(fullfile(ExtFuncs,Name,'install'));
+
+            % Run cmake again
+            disp('... running cmake to create project external function');
+            cd( buildFolder);
+            textcommand1 = ['cmake ' CgenDir1 ' -G "' Compiler '" -DTARGET_NAME:STRING=' Name];
+            system(textcommand1);
+            textcommand2 = 'cmake --build . --config RelWithDebInfo --target install';
+            system(textcommand2);
+
+            %% Copy the .dll files to your predictive simulation folder
+            %     copyfile(fullfile(ExtFuncs,Name,'build','RelWithDebInfo',[Name '.dll ']),DllPath);
+            copyfile(fullfile(ExtFuncs,Name,'install','bin',[Name '.dll ']),DllPath);
+            cd(PathStart);
+        catch
+            % restore the original cmakelist file if an error occured
+            CmakeFile = fullfile(OsimSource,'OpenSim\External_Functions','CMakeLists.txt');
+            tempCopy = fullfile(pwd,'CMakeListTempCopy.txt');
+            copyfile(tempCopy,CmakeFile);
+            delete(tempCopy)
+            % go back to startpath
+            cd(PathStart);
+        end
+    else
+        disp([ExtFuncPath ' does exist and is already an opensim project'])
     end
-    system([Name '.exe']);
-    copyfile(fullfile(ExePath,'foo.m'),fullfile(CgenDir1,'foo.m'));
-    delete(fullfile(ExePath,'foo.m'));
-    
-    % - third: create the foo_jac.m using a matlab function
-    disp('... Creating foo_jac.c');
-    generate_foo_jac(nInputDll,CgenDir1);
-    
-    %% Run cmake to build external function project
-    
-    % First: create the cmake file
-    fid = fopen(fullfile(CgenDir1,'CMakeLists.txt'),'wt');
-    fprintf( fid, '%s\n', 'cmake_minimum_required (VERSION 2.8)');
-    fprintf( fid, '%s\n', 'set (CMAKE_CXX_STANDARD 11)');
-    fprintf( fid, '%s\n', ['set(TEST_TARGET ' Name ')']);
-    fprintf( fid, '%s\n', 'project(${TEST_TARGET})');
-    fprintf( fid, '%s\n', 'add_library(${TEST_TARGET} SHARED foo_jac.c)');
-    fprintf( fid, '%s\n', 'install(TARGETS ${TEST_TARGET}');
-    fprintf( fid, '%s\n', '	PERMISSIONS OWNER_READ OWNER_WRITE OWNER_EXECUTE');
-    fprintf( fid, '%s\n', '				GROUP_READ GROUP_WRITE GROUP_EXECUTE');
-    fprintf( fid, '%s\n', '				WORLD_READ WORLD_EXECUTE');
-    fprintf( fid, '%s\n', '	LIBRARY DESTINATION lib');
-    fprintf( fid, '%s\n', '	ARCHIVE DESTINATION lib');
-    fprintf( fid, '%s\n', '	RUNTIME DESTINATION bin');
-    fprintf( fid, '%s\n', '	)');
-    fclose(fid);
-    
-    % second: create folder for external functions
-    mkdir(fullfile(ExtFuncs,Name,'build'));
-    mkdir(fullfile(ExtFuncs,Name,'install'));
-    
-    % Run cmake again
-    disp('... running cmake to create project external function');
-    cd(fullfile(ExtFuncs,Name,'build'));
-    TextCommand = ['cmake ' CgenDir1 ' -G "' Compiler '"'];
-    system(TextCommand);
-    
-    
-    %% create .dll file using the visual studio
-    
-    % go to path with executable to run visual studio from command line
-    disp('... Build and install external func project in visual studio ');
-    cd(ExeDevPath);
-    
-    % Build and install project in visual studio
-    ExtFunc_Build = fullfile(ExtFuncs,Name,'build');
-    TextCommand = ['devenv.exe ' ExtFunc_Build '\' Name '.sln /Build ' 'RelWithDebInfo ' '/Project ALL_BUILD' ];
-    system(TextCommand);
-    TextCommand = ['devenv.exe ' ExtFunc_Build '\' Name '.sln /Build ' 'RelWithDebInfo ' '/Project INSTALL' ];
-    system(TextCommand);
-    
-    %% Copy the .dll files to your predictive simulation folder    
-    copyfile(fullfile(ExtFuncs,Name,'build','RelWithDebInfo',[Name '.dll ']),DllPath);
-    cd(PathStart);
 
 else
     %% warning message
--- ExampleScript.m
+++ ExampleScript.m
@@ -2,16 +2,17 @@
 
 
 % path to cpp file
-CppDir = 'C:\Users\u0088756\Documents\FWO\Software\GitProjects\CreateDll_PredSim';  % directory of cpp file
-Name = 'PredSim_v2_Bram'; % name of the CPP file
+CppDir = 'C:\Users\mat950\Documents\Software\Sim\CreateDll_PredSim';  % directory of cpp file
+Name = 'PredSim_vTest'; % name of the CPP file
 
 % additional path information
-OsimSource = 'C:\opensim-ad-core-source';   % source code of opensim (with AD)
-OsimBuild = 'C:\GBW_MyPrograms\opensim-ad-core-build2';    % build of opensim
-DllPath = 'C:\Users\u0088756\Documents\FWO\Software\ExoSim\SimExo_3D\3dpredictsim\ExternalFunctions'; % copies .dll file to this directory
-ExtFuncs = 'C:\opensim-ExternalFunc';           % directory where you build the binaries for the external functions
-VSinstall = 'C:\Program Files (x86)\Microsoft Visual Studio 14.0';  % directory installation visual studio
-Compiler = 'Visual Studio 14 2015 Win64';       % compiler 
+OsimSource = 'C:\opensim-ad\opensim-ad-core';   % source code of opensim (with AD)
+OsimBuild = 'C:\opensim-ad\build';    % build of opensim
+DllPath = 'C:\Users\mat950\Documents\Software\Sim\CreateDll_PredSim'; % copies .dll file to this directory
+ExtFuncs = 'C:\opensim-ad\ExternalFuncInst';           % directory where you build the binaries for the external functions
+VSinstall = 'C:\Program Files (x86)\Microsoft Visual Studio\2017\Community';  % directory installation visual studio
+% Compiler = 'Visual Studio 15 2017 Win64';       % compiler 
+Compiler = 'Visual Studio 15 2017';       % compiler 
 
 % number of input arguments for external function
 nInputDll = 93;
